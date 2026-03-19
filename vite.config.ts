e01a6a1f5ff45068a03a1215960cbc9345007e9f
Oleg Pervuhin <oleggg89039128471@gmail.com>
2026-03-19 10:14:33 +0700

    сделал 6 лабу

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        include: ['lab1/**/*.test.ts', '**/*.test.ts'],
+        include: ['test/**/*.ts', 'tests/**/*.test.ts'],
     },
 })
